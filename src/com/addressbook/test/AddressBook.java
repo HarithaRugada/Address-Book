bb02371fcc8e3ac44d9c51e225edc4129c23b636
HarithaRugada <haritharugada@gmail.com>
2020-06-17 22:19:46 +0530

    ADDED : Add Multiple Persons to Address Book

--- src/com/addressbook/test/AddressBook.java
+++ src/com/addressbook/test/AddressBook.java
@@ -12,27 +12,34 @@ public class AddressBook {
 
         System.out.println("Welcome to Address Book Program");
         Scanner Sc=new Scanner(System.in);
-        System.out.println("Enter your choice");
-        int choice=Sc.nextInt();
-        switch(choice)
+        System.out.println("Add Person");
+        System.out.println("Edit Person");
+        System.out.println("Delete Person");
+        System.out.println("Exit");
+        while(true)
         {
-            case 1:
-                System.out.println("Add Person");
-                addressBookManager.addPerson(null);
-                break;
+            System.out.println("Enter your choice");
+            int choice = Sc.nextInt();
+            switch (choice)
+            {
+                    case 1:
+                        addressBookManager.addPerson(null);
+                        break;
 
-            case 2:
-                System.out.println("Edit Person");
-                addressBookManager.editPerson(null);
-                break;
+                    case 2:
+                        addressBookManager.editPerson(null);
+                        break;
 
-            case 3:
-                System.out.println("Delete Person");
-                addressBookManager.deletePerson(null);
-                break;
+                    case 3:
+                        addressBookManager.deletePerson(null);
+                        break;
 
-            default:
-                System.out.println("Wrong Choice");
+                    case 4:
+                        System.exit(0);
+
+                    default:
+                        System.out.println("Wrong Choice");
+            }
         }
     }
 }
